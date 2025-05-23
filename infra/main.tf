787777f3f23242011dee79f4f968a067749dc750
Rounak Pashine <39852112+rounakpashine@users.noreply.github.com>
2025-05-23 16:45:15 +0530

    Fix: Reformat main.tf to satisfy terraform fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
-  source      = "./modules/s3_bucket"
-  bucket_name = var.bucket_name
-  acl         = var.acl
+  source        = "./modules/s3_bucket"
+  bucket_name   = var.bucket_name
+  acl           = var.acl
   force_destroy = var.force_destroy
-  tags        = var.tags
+  tags          = var.tags
 }
