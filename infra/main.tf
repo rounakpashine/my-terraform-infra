21a813539df72583c97041e34701034e2ef1bbe8
Rounak Pashine <39852112+rounakpashine@users.noreply.github.com>
2025-05-23 17:48:05 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,21 +1,8 @@
-# Terraform Configuration for S3 Bucket with Random Name
-
 provider "aws" {
   region = "us-east-1"
 }
 
-# Generate a random suffix for the S3 bucket name
-resource "random_pet" "bucket_suffix" {
-  length    = 2
-  separator = "-"
-}
-
-# Create the S3 bucket with a random name ending in "test-wk"
-resource "aws_s3_bucket" "this" {
-  bucket = "${random_pet.bucket_suffix.id}-test-wk"
+resource "aws_s3_bucket" "simple" {
+  bucket = "my-simple-s3-bucket-test-wk"
   acl    = "private"
-
-  # Additional configuration options can be added here
 }
-
-# Optionally, you can add other resources or configurations as needed
